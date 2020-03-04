a6bd3e02cc60715027c7014dbca85ed3e8412fa8
Rob <rob_cord@aol.com>
2020-03-04 12:23:02 -0700

    latest classwork changes

---
+++ McSnk/Assets/Scripts/PlayerPawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPawn : Pawn
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Attack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+        Debug.Log("Player Attack");
+        base.Attack();
+    }
+}
